211a3c4d6f71b45a27659504d25f01e4d9bf5a87
Daniel Espendiller <daniel@espendiller.net>
2015-07-14 20:07:05 +0200

    prepare travis infrastructure

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 Intellij / PhpStorm Plugin for PHP Improvements
 ========================
 
+[![Build Status](https://travis-ci.org/Haehnchen/idea-php-toolbox.svg?branch=master)](https://travis-ci.org/Haehnchen/idea-php-toolbox)
+[![Version](http://phpstorm.espend.de/badge/xxxx/version)](https://plugins.jetbrains.com/plugin/xxxx)
+[![Downloads](http://phpstorm.espend.de/badge/xxxx/downloads)](https://plugins.jetbrains.com/plugin/xxxx)
+[![Downloads last month](http://phpstorm.espend.de/badge/xxxx/last-month)](https://plugins.jetbrains.com/plugin/xxxx)
+
 Url : http://plugins.jetbrains.com/plugin?pr=&pluginId=xxx
+
 ID: de.espend.idea.php.toolbox
+
 Doc: http://phpstorm.espend.de/php-toolbox
 
 ## Json Configurator
@@ -16,3 +23,4 @@ Doc: http://phpstorm.espend.de/php-toolbox
 ## Remote Storage
 
 ## Other
+ * PHP: Improvements in type hint variable completion
---
+++ travis.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+ideaVersion="14.1.4"
+travisCache=".cache"
+
+if [ ! -d ${travisCache} ]; then
+    echo "Create cache" ${travisCache} 
+    mkdir ${travisCache}
+fi
+
+function download {
+
+  url=$1
+  basename=${url##*[/|\\]}
+  cachefile=${travisCache}/${basename}
+  
+  if [ ! -f ${cachefile} ]; then
+      wget $url -P ${travisCache};
+    else
+      echo "Cached file `ls -sh $cachefile` - `date -r $cachefile +'%Y-%m-%d %H:%M:%S'`"
+  fi  
+
+  if [ ! -f ${cachefile} ]; then
+    echo "Failed to download: $url"
+    exit 1
+  fi  
+}
+
+# Unzip IDEA
+
+if [ -d ./idea  ]; then
+  rm -rf idea
+  mkdir idea
+  echo "created idea dir"  
+fi
+
+# Download main idea folder
+download "http://download.jetbrains.com/idea/ideaIU-${ideaVersion}.tar.gz"
+tar zxf ${travisCache}/ideaIU-${ideaVersion}.tar.gz -C .
+
+# Move the versioned IDEA folder to a known location
+ideaPath=$(find . -name 'idea-IU*' | head -n 1)
+mv ${ideaPath} ./idea
+  
+if [ -d ./plugins ]; then
+  rm -rf plugins
+  mkdir plugins
+  echo "created plugin dir"  
+fi
+
+#php
+download "https://plugins.jetbrains.com/files/6610/20075/php-141.1534.zip"
+unzip -qo $travisCache/php-141.1534.zip -d ./plugins
+        
+#twig
+download "http://plugins.jetbrains.com/files/7303/17519/twig-139.58.zip"
+unzip -qo $travisCache/twig-139.58.zip -d ./plugins
+
+# Run the tests
+if [ "$1" = "-d" ]; then
+    ant -d -f build-test.xml -DIDEA_HOME=./idea
+else
+    ant -f build-test.xml -DIDEA_HOME=./idea
+fi
+
+# Was our build successful?
+stat=$?
+
+if [ "${TRAVIS}" != true ]; then
+    ant -f build-test.xml -q clean
+
+    if [ "$1" = "-r" ]; then
+        rm -rf idea
+        rm -rf plugins
+    fi
+fi
+
+# Return the build status
+exit ${stat}
