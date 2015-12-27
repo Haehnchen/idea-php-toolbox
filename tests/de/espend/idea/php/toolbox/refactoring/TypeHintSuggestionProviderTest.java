2dafea847d8f004e6e92f64eb5ebe6a0592139e9
Daniel Espendiller <daniel@espendiller.net>
2015-12-27 19:38:16 +0100

    add tests for type hint variables

---
+++ tests/de/espend/idea/php/toolbox/refactoring/TypeHintSuggestionProviderTest.java
@@ -0,0 +1,32 @@
+package de.espend.idea.php.toolbox.refactoring;
+
+import com.jetbrains.php.lang.PhpFileType;
+import de.espend.idea.php.toolbox.SymfonyLightCodeInsightFixtureTestCase;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * @author Daniel Espendiller <daniel@espendiller.net>
+ *
+ * @see TypeHintSuggestionProvider
+ */
+public class TypeHintSuggestionProviderTest extends SymfonyLightCodeInsightFixtureTestCase {
+
+    public void testTypeHintVariableCompletionExtendsListWithStrippedWordList() {
+        Collection<String[]> providers = Arrays.asList(
+            new String[] {"FooBarInterface", "fooBar"},
+            new String[] {"FooBarInterface", "bar"},
+            new String[] {"FooBarAbstract", "fooBar"},
+            new String[] {"FooAbstract", "foo"}
+        );
+
+        for (String[] provider : providers) {
+            assertCompletionContains(
+                PhpFileType.INSTANCE,
+                String.format("<?php function f(%s $<caret>);", provider[0]),
+                "$" + provider[1]
+            );
+        }
+    }
+}
