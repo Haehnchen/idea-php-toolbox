5625c86acdfb021207ff5f0ea05a22d1f7a568fc
Cedric Ziel <cedric@cedric-ziel.com>
2018-03-16 13:22:36 +0100

    Adjust icon location to new structure

--- src/main/java/de/espend/idea/php/toolbox/PhpToolboxIcons.java
+++ src/main/java/de/espend/idea/php/toolbox/PhpToolboxIcons.java
@@ -8,5 +8,5 @@ import javax.swing.*;
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
 public class PhpToolboxIcons {
-    public static final Icon TOOLBOX = IconLoader.getIcon("icons/toolbox.png");
+    public static final Icon TOOLBOX = IconLoader.getIcon("/icons/toolbox.png");
 }
