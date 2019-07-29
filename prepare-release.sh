2711bfcfba044d9808498c52eda9a7c26ebbd7cb
Daniel Espendiller <daniel@espendiller.net>
2019-07-29 17:09:44 +0200

    update gradle build

---
+++ prepare-release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+git submodule update --init --recursive
+
+echo "<html><ul>" > change-notes.html
+git log `git describe --tags --abbrev=0`..HEAD --no-merges --oneline --pretty=format:"<li>%h %s (%an)</li>" >> change-notes.html
+echo "</ul></html>" >> change-notes.html
+
+cp change-notes.html src/main/resources/META-INF/
+
+rm change-notes.html
