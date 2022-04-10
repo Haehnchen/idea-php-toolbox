e8cf05b56324b622993e6e0f3a941471e723319c
Daniel Espendiller <daniel@espendiller.net>
2022-04-10 14:25:38 +0200

    prepare dynamic plugin feature

--- src/main/java/de/espend/idea/php/toolbox/PhpToolboxProjectComponent.java
+++ src/main/java/de/espend/idea/php/toolbox/PhpToolboxProjectComponent.java
@@ -1,35 +1,33 @@
 package de.espend.idea.php.toolbox;
 
-import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import de.espend.idea.php.toolbox.remote.util.PersistentStorageUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Daniel Espendiller <daniel@espendiller.net>
  */
-public class PhpToolboxProjectComponent implements ProjectComponent {
-
-    private Project project;
-
-    public PhpToolboxProjectComponent(@NotNull Project project) {
-        this.project = project;
+public class PhpToolboxProjectComponent {
+    public static class PostStartupActivity implements StartupActivity {
+        @Override
+        public void runActivity(@NotNull Project project) {
+            DumbService.getInstance(project).smartInvokeLater(() -> PersistentStorageUtil.load(project));
+        }
     }
 
-    @Override
-    public void projectOpened() {
-        DumbService.getInstance(project).smartInvokeLater(() -> PersistentStorageUtil.load(project));
-    }
+    public static class ProjectCloseService implements Disposable {
+        private final Project project;
 
-    @Override
-    public void projectClosed() {
-        PersistentStorageUtil.write(project);
-    }
+        public ProjectCloseService(@NotNull Project project) {
+            this.project = project;
+        }
 
-    @NotNull
-    @Override
-    public String getComponentName() {
-        return "Php-Toolbox";
+        @Override
+        public void dispose() {
+            PersistentStorageUtil.write(project);
+        }
     }
 }
