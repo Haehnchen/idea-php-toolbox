3a95127acba59677dc0ff63fe035ff856c7566b5
Daniel Espendiller <daniel@espendiller.net>
2022-04-11 10:05:58 +0200

    remove http server features

--- src/main/java/de/espend/idea/php/toolbox/remote/MessageNotifier.java
+++
@@ -1,8 +0,0 @@
-package de.espend.idea.php.toolbox.remote;
-
-/**
- * @author Daniel Espendiller <daniel@espendiller.net>
- */
-public interface MessageNotifier extends Runnable {
-    void addMessageHandler(MessageHandler handler);
-}
